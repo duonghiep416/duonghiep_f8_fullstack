4c50a00e8db5a84cda0f7cf1072e1c88374bc0ae
duonghiep416 <duonghiep416@gmail.com>
2023-09-17 06:40:57 +0700

    Submit exercises Day 29

--- Day29/main.js
+++ Day29/main.js
@@ -1,172 +1,39 @@
-const allLesson = [
-    "Nhập môn lập trình web",
-    "Giới thiệu khóa học HTML, CSS",
-    "Lập trình web là gì? Thiết kế web là gì?",
-    "Lộ trình học lập trình web Fullstack",
-    "Phương pháp học lập trình hiệu quả",
-    "Giới thiệu về ngôn ngữ HTML",
-    "Cấu trúc trang HTML, cấu trúc thẻ HTML, cách học và nhớ các thẻ HTML",
-    "Các loại thẻ HTML, các nhóm thẻ HTML",
-    "Các thẻ HTML hay dùng trong nhóm Block",
-    "Các thẻ HTML hay dùng trong nhóm Inline",
-    "Giới thiệu về ngôn ngữ CSS",
-    "Cấu trúc trang CSS, cấu trúc thẻ CSS, cách học và nhớ các thẻ CSS",
-    "Các loại thẻ CSS, các nhóm thẻ CSS",
-    "Các thẻ CSS hay dùng trong nhóm Block",
-    "Các thẻ CSS hay dùng trong nhóm Inline",
-];
-const titleModule = [
-    "Nhập môn lập trình web",
-    "Giới thiệu về ngôn ngữ HTML",
-    "Giới thiệu về ngôn ngữ CSS",
-];
-let currentModule = null;
-let module;
-function createModule() {
-    module = [];
-    let lessonName = [];
-    allLesson.forEach((lesson, index) => {
-        if (titleModule.includes(lesson)) {
-            // Nếu bắt đầu một module mới
-            currentModule = {
-                id: module.length + 1,
-                nameModule: lesson,
-                lesson: [],
-            };
-            module.push(currentModule);
-        }
-        // Nếu là bài học trong module hiện tại
-        else if (currentModule) {
-            currentModule.lesson.push({ nameLesson: lesson });
-        } else if (!currentModule && !titleModule.includes(lesson)) {
-            module[0] = {
-                id: 1,
-                nameModule: null,
-                lesson: lessonName,
-            };
-            module[0].lesson.push({ nameLesson: lesson });
-        }
-    });
-}
-
-function renderCourseContent() {
-    let indexLesson = 0;
-    const courseContent = document.querySelector(".course-content");
-    const html = module
-        .map((item, index) => {
-            return `<li class="course-module">
-                    <p class="module-text course-lesson" draggable="true">${
-                        "Module " +
-                        (module[0].nameModule ? index + 1 : index) +
-                        ": "
-                    } <span>${item.nameModule}</span></p>
-                    <ul class="course-lesson-container">
-                        ${item.lesson
-                            .map(
-                                (
-                                    lesson
-                                ) => `<li class="course-lesson" draggable="true">
-                            <p class="lesson-text">Bài ${++indexLesson}: ${
-                                    lesson.nameLesson
-                                }</p>
-                        </li>`
-                            )
-                            .join("")}
-                    </ul>
-                </li>`;
-        })
-        .join("");
-
-    courseContent.innerHTML = `<ul>${html}</ul>`;
-
-    const firstLesson = document.querySelector(
-        ".course-module .course-lesson span"
-    );
-    const firstCourseLesson = document.querySelector(
-        ".course-module .course-lesson"
-    );
-    if (firstLesson.innerHTML === "null") {
-        firstCourseLesson.style.display = "none";
-    }
-}
-
-function createPlaceholder() {
-    const placeholder = document.createElement("div");
-    placeholder.className = "placeholder";
-    return placeholder;
-}
-
-function dragLesson() {
-    const courseContent = document.querySelector(".course-content");
-    const heightItem = document.querySelector(".course-lesson").offsetHeight;
-    const courseContentTop = courseContent.offsetTop;
-    let oldLocation = 0;
-    let newLocation = 0;
-    let draggingItem = null;
-    let placeholder = null;
-
-    courseContent.addEventListener("dragstart", (e) => {
-        const item = e.target.closest(".course-lesson");
-        if (!item) return;
-        item.classList.add("dragging");
-        draggingItem = item;
-        const offsetTop = item.offsetTop;
-        oldLocation = Math.floor((offsetTop - courseContentTop) / heightItem);
+const courseModule = document.querySelector(".course-module");
+const courseLessonList = document.querySelectorAll(".course-lesson");
+let currentDraggedElement = null;
+courseLessonList.forEach((lesson) => {
+    lesson.addEventListener("dragstart", function (e) {
+        currentDraggedElement = this;
     });
 
-    courseContent.addEventListener("dragover", (e) => {
+    lesson.addEventListener("dragover", function (e) {
         e.preventDefault();
-        if (!draggingItem) return;
-
-        const item = e.target.closest(".course-lesson");
-        if (!item) return;
-
-        if (item === placeholder) return;
-
-        const offsetTop = item.offsetTop;
-        newLocation = Math.floor((offsetTop - courseContentTop) / heightItem);
-        if (newLocation !== oldLocation) {
-            const itemMove = allLesson.splice(oldLocation, 1)[0];
-            allLesson.splice(newLocation, 0, itemMove);
-            createModule();
-            renderCourseContent();
-            item.classList.remove("dragging");
-            oldLocation = newLocation;
-        }
+        if (currentDraggedElement === this) return;
+        currentDraggedElement.parentNode.removeChild(currentDraggedElement);
+        courseModule.insertBefore(currentDraggedElement, this);
+        updateOrder();
     });
 
-    courseContent.addEventListener("dragenter", (e) => {
-        if (!draggingItem) return;
-
-        const item = e.target.closest(".course-lesson");
-        if (!item) return;
-
-        if (item === placeholder) return;
-
-        if (placeholder && placeholder.parentNode === courseContent) {
-            courseContent.removeChild(placeholder);
-        }
-
-        placeholder = createPlaceholder();
-        if (
-            item.parentNode === courseContent &&
-            placeholder.parentNode === courseContent
-        ) {
-            courseContent.insertBefore(placeholder, item);
-        }
+    lesson.addEventListener("drop", function (e) {
+        e.preventDefault();
+        updateOrder();
     });
+});
 
-    courseContent.addEventListener("dragend", () => {
-        if (placeholder && placeholder.parentNode === courseContent) {
-            courseContent.removeChild(placeholder);
-        }
-
-        draggingItem = null;
-        createModule();
-        renderCourseContent();
+function updateOrder() {
+    const textLessonList = document.querySelectorAll(".text-lesson");
+    const moduleList = document.querySelectorAll(".module-text");
+    const contentTextLesson = document.querySelectorAll(".text-lesson span");
+    const contentModule = document.querySelectorAll(".module-text span");
+    textLessonList.forEach(function (textLesson, index) {
+        textLesson.innerHTML = `Bài ${index + 1}: <span>${
+            contentTextLesson[index].innerHTML
+        }</span>`;
+    });
+    moduleList.forEach(function (module, index) {
+        module.innerHTML = `Module ${index + 1}: <span>${
+            contentModule[index].innerHTML
+        }</span>`;
     });
 }
-
-createModule();
-renderCourseContent();
-dragLesson();
+updateOrder();
