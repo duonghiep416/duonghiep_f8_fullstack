27c7b4495c73f4b4e52c2960aaf7229f78a94160
duonghiep416 <duonghiep416@gmail.com>
2023-10-06 15:48:00 +0700

    Add CSS exercises Day 35

--- Day35/js/main.js
+++ Day35/js/main.js
@@ -21,15 +21,42 @@ getPosts({ _limit: limitPostInPage, _page: 1 });
 const render = (posts) => {
     if (posts.length) {
         posts.forEach(
-            ({ id, name, title, excerpt, content, thumbnail_url }) => {
+            ({ id, user, title, excerpt, content, thumbnail_url }) => {
                 const postItem = document.createElement("div");
                 postItem.classList.add("post-item");
                 const h2 = document.createElement("h2");
+                h2.classList.add("post-title");
                 h2.innerText = title;
                 postItem.append(h2);
+                const userInfo = document.createElement("div");
+
+                userInfo.classList.add("user-info");
+                const imgContainer = document.createElement("div");
+                imgContainer.classList.add("img-container");
+                const img = document.createElement("img");
+                img.classList.add("avatar-img");
+                if (user.is_pro) {
+                    img.classList.add("pro");
+                    const crown = document.createElement("img");
+                    crown.src =
+                        "https://fullstack.edu.vn/static/media/crown.8edf462029b3c37a7f673303d8d3bedc.svg";
+                    crown.classList.add("crown");
+                    imgContainer.append(crown);
+                }
+                img.src = user.avatar_url;
                 const h3 = document.createElement("h3");
-                h3.innerText = name;
-                postItem.append(h3);
+                h3.innerText = user.name;
+                imgContainer.append(img);
+                userInfo.append(imgContainer);
+                userInfo.append(h3);
+                if (user.is_admin) {
+                    userInfo.classList.add("admin");
+                    const blueTick = document.createElement("img");
+                    blueTick.classList.add("blue-tick");
+                    blueTick.src = "./icon/blue-tick.svg";
+                    userInfo.append(blueTick);
+                }
+                postItem.append(userInfo);
                 const divContent = document.createElement("div");
                 const htmlContent = marked.parse(content);
                 divContent.innerHTML = htmlContent;
