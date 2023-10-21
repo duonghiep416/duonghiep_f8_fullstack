9a55db89d99d366fdf9bb0a520d874126dfa5f7f
duonghiep416 <duonghiep416@gmail.com>
2023-10-21 14:44:00 +0700

    Fix bug regex

--- Day37/js/regex.js
+++ Day37/js/regex.js
@@ -3,11 +3,13 @@ const emailPattern =
     /([a-zA-Z]{1,}[a-zA-Z-0-9\._-]{5,}@[a-z0-9-_\.]+\.[a-z]{2,})/g;
 const youtubePattern =
     /(?:http|https):\/\/www\.(?:youtube\.com\/watch\?v=([\w-]+)|youtu\.be\/([\w-]+))&*(\<|.*? )/g;
-
+const linkRegex =
+    /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*))/g;
 export function convertRegex() {
     const articleContainer = document.querySelector(".article-container");
     let html = articleContainer.innerHTML;
     html = html
+        .replace(linkRegex, "<a href='$1' class='link' target='_blank'>$1</a>")
         .replace(
             emailPattern,
             "<a href='mailto:$1' class='email-link' target='_blank'>$1</a>"
@@ -21,7 +23,14 @@ export function convertRegex() {
             `<iframe width="600" height="315" src="https://www.youtube.com/embed/$2$1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>${
                 youtubePattern.exec(html)[3] === "<" ? "$3" : ""
             }`
-        );
-
+        )
+        .replaceAll(
+            `style="margin-top: 10px;">`,
+            `<hr style="margin-top: 10px;">`
+        )
+        .replaceAll(`br>`, "<br>")
+        .replaceAll("/a>", "</a>")
+        .replaceAll("<</a>", "</a>")
+        .replaceAll(`<<br>`, "<br>");
     articleContainer.innerHTML = html;
 }
