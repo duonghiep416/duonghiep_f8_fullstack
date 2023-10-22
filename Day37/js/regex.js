5f845278d7a40341bcf8347896116334f1346756
duonghiep416 <duonghiep416@gmail.com>
2023-10-22 15:25:20 +0700

    Fix bug

--- Day37/js/regex.js
+++ Day37/js/regex.js
@@ -2,18 +2,31 @@ const phonePattern = /((0|\+84)[0-9]{9})/g;
 const emailPattern =
     /([a-zA-Z]{1,}[a-zA-Z-0-9\._-]{5,}@[a-z0-9-_\.]+\.[a-z]{2,})/g;
 const youtubePattern =
-    /(?:http|https):\/\/www\.(?:youtube\.com\/watch\?v=([\w-]+)|youtu\.be\/([\w-]+))&*(\<|.*? )/g;
+    /(?:(?:http|https):\/\/www\.)?(?:youtube\.com\/watch\?v=([\w-]+)|youtu\.be\/([\w-]+))&*(<|.*?(<| ))/g;
 const linkRegex =
-    /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*))/g;
+    /(((https?:\/\/)?(www\.)?)?([-a-zA-Z0-9:%._\+~#=;]{1,256}\.[a-zA-Z0-9())]{1,6}\b([-a-zA-ZàáãạảăắằẳẵặâấầẩẫậèéẹẻẽêềếểễệđìíĩỉịòóõọỏôốồổỗộơớờởỡợùúũụủưứừửữựỳỵỷỹýÀÁÃẠẢĂẮẰẲẴẶÂẤẦẨẪẬÈÉẸẺẼÊỀẾỂỄỆĐÌÍĨỈỊÒÓÕỌỎÔỐỒỔỖỘƠỚỜỞỠỢÙÚŨỤỦƯỨỪỬỮỰỲỴỶỸÝ0-9()@:%_\+.~#?&\/\/=;]*)))/g;
+
+const deleteSpaceRegex = /( ){2,}|(<br>){3,}/g;
 export function convertRegex() {
     const articleContainer = document.querySelector(".article-container");
     let html = articleContainer.innerHTML;
+    console.log(html);
     html = html
-        .replace(linkRegex, "<a href='$1' class='link' target='_blank'>$1</a>")
+        .replace(deleteSpaceRegex, "$1$2")
+        .replaceAll("@gmail.com", "@gmail. com")
+        .replace(
+            linkRegex,
+            `<a href='${
+                "$1".startsWith("https://") ? "$5" : "https://$5"
+            }' class='link' target='_blank'>$5</a>`
+        )
+        .replaceAll("@gmail. com", "@gmail.com")
+
         .replace(
             emailPattern,
             "<a href='mailto:$1' class='email-link' target='_blank'>$1</a>"
         )
+
         .replace(
             phonePattern,
             "<a href='tel:$1' class='phone-link' target='_blank'>$1</a>"
@@ -21,7 +34,7 @@ export function convertRegex() {
     if (youtubePattern.exec(html)) {
         html = html.replace(
             youtubePattern,
-            `<iframe width="600" height="315" src="https://www.youtube.com/embed/$2$1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>${
+            `<iframe width="560" height="315" src="https://www.youtube.com/embed/$2$1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>${
                 youtubePattern.exec(html)[3] === "<" ? "$3" : ""
             }`
         );
