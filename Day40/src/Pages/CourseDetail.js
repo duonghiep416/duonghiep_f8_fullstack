c6cf07d059c394d24091118bcfbcabcf3e2dd1d3
duonghiep416 <duonghiep416@gmail.com>
2023-10-25 16:24:41 +0700

    Fix bug images

--- Day40/src/Pages/CourseDetail.js
+++ Day40/src/Pages/CourseDetail.js
@@ -1,16 +1,15 @@
 import { coursesDetail } from "./Courses";
+import qrCode from "../Assets/images/qr-code.jpg";
 export const CourseDetail = ({ data }) => {
     const course = coursesDetail.find((course) => course.path === data.id);
     return `
-        <h1>Chi tiết khóa học: ${course.name}</h1>
+        <h2 class="mt-4">Chi tiết khóa học: <span class="font-bold">${
+            course.name
+        }</span></h2>
         <p class='text-lg font-bold'>${
             course.isPublished ? "" : "Khóa học chưa được ra mắt"
         }</p>
-        ${
-            course.isPro && course.isPublished
-                ? "Hello <img src='./src/Assets/images/f8-icon.png'/>"
-                : ""
-        }
+        <p class='text-lg font-bold'>Vui lòng liên hệ qua stk để nhận thông tin chi tiết khóa học!</p><img src='${qrCode}' alt='qr-code' class='w-80'/>
         <a href="/courses" data-navigo class="bg-orange-400 rounded-full px-4 py-3 font-semibold text-lg inline-block mt-3 text-white">Back</a>
         `;
 };
